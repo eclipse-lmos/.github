58f7708551849feae50550584015ff8495b41e70
Patrick Schwager <patrick.schwager@telekom.de>
2025-02-07 14:21:38 +0100

    x

--- scripts/prepeareRelease.sh
+++ scripts/prepeareRelease.sh
@@ -1,6 +1,8 @@
 repo="$2"
 releaseVersion="$1"
 sed "s/^version[[:blank:]]*=.*$/version=$version/" "${repo}/gradle.properties"
+git config --global user.email "cicd@ancf.ai"
+git config --global user.name "CICD
 git -C "${repo}" commit -a -m "Release: ${releaseVersion}"
 git -C "${repo}" tag -a "${releaseVersion}" -m "${releaseVersion}"
 git -C "${repo}" push origin "${releaseVersion}"
