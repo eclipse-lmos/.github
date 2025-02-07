ebc357f85329993d27d1e4c197e36415c6b43c14
Patrick Schwager <patrick.schwager@telekom.de>
2025-02-07 14:24:03 +0100

    x

--- scripts/prepeareRelease.sh
+++ scripts/prepeareRelease.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 
 repo="$2"
 releaseVersion="$1"
-sed "s/^version[[:blank:]]*=.*$/version=$version/" "${repo}/gradle.properties"
+sed -i "s/^version[[:blank:]]*=.*$/version=${releaseVersion}/" "${repo}/gradle.properties"
 git config --global user.email "cicd@ancf.ai"
 git config --global user.name "CICD"
 git -C "${repo}" commit -a -m "Release: ${releaseVersion}"
