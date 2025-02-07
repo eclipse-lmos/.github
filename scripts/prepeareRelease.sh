803b0b6caedeed2818bd5df4ce7f728ec1aec67a
Patrick Schwager <patrick.schwager@telekom.de>
2025-02-07 14:04:31 +0100

    Add release pipelines

---
+++ scripts/prepeareRelease.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+set -e -o pipefail
+
+repo="$2"
+releaseVersion="$1"
+sed -i "s/^version[[:blank:]]*=.*$/version=${releaseVersion}/" "${repo}/gradle.properties"
+git config --global user.email "cicd@ancf.ai"
+git config --global user.name "CICD"
+git -C "${repo}" commit -a -m "Release: ${releaseVersion}"
+git -C "${repo}" tag -a "${releaseVersion}" -m "${releaseVersion}"
+git -C "${repo}" push origin "${releaseVersion}"
